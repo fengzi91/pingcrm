00f5839418b0bae9405689c35301526b45e2a3e5
zys <fengzi91@vip.qq.com>
2021-07-18 17:04:04 +0800

    添加 docker 环境和中文文档地址

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
     <p class="mb-8 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo-500 underline hover:text-orange-600" href="https://inertiajs.com">Inertia.js</a> works.</p>
+    <p class="mb-8 leading-normal">中文文档地址：<a class="text-indigo-500 underline hover:text-orange-600" href="https://inertiajs.zhitiantu.com">Inertia.js</a></p>
   </div>
 </template>
 
